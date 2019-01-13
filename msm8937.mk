182dea3c66292a6e5fc5b28e3c3cc542b1b00e00
Joey <joey@lineageos.org>
2019-01-13 10:21:11 +0100

    msm8937-common: Build Trust HAL
    Change-Id: I92706987f8d3094cdfb82e71e5331b7efe69e5df
    Signed-off-by: Joey <joey@lineageos.org>

--- msm8937.mk
+++ msm8937.mk
@@ -397,6 +397,10 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     vendor.lineage.touch@1.0-service.xiaomi_8937
 
+# Trust HAL
+PRODUCT_PACKAGES += \
+    vendor.lineage.trust@1.0-service
+
 # USB
 PRODUCT_PACKAGES += \
     android.hardware.usb@1.0-service.basic
